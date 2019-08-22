0c50af001cf861ba3b6210d8901bbae5a7a0da62
Gabriele Cerfoglio <gabriele.cerfoglio@martel-innovate.com>
2019-08-22 16:07:30 +0200

    Comments, fix for previous state assumption

--- occupancy.py
+++ occupancy.py
@@ -4,6 +4,7 @@ import sys, getopt, datetime, time, math
 from crate import client
 
 def main(argv):
+    # Setting up the connection to CrateDB (with command line args)
     crate_host = ''
     crate_user = ''
     try:
@@ -18,17 +19,26 @@ def main(argv):
             crate_user = arg
     connection = client.connect(crate_host, username=crate_user)
     cursor = connection.cursor()
+
+    # Getting current time, and previous day time
     currentTime = datetime.datetime.now().replace(microsecond=0,second=0,minute=0)
     previousDay = (currentTime - datetime.timedelta(hours=24))
+
+    # Setting up cursor and pulling data from the last 24 hours
     cursor.execute('SELECT "status","time_index", "entity_id", "entity_type", "fiware_servicepath" FROM "mtekz"."etparkingspot" WHERE "time_index">=? ORDER BY time_index ASC', (previousDay.strftime('%s')+'000',))
     data = cursor.fetchall()
+
+    # List of Service Paths
     servicePaths = list(dict.fromkeys(map(lambda a: a[4], data)))
+
+    # Computing occupancy data for all entities and servicepaths
     occupancyData = []
     for path in servicePaths:
         pathData = filter(lambda a: a[4] == path, data)
         entityIds = list(dict.fromkeys(map(lambda a: a[2], pathData)))
         for entity in entityIds:
-            cursor.execute('SELECT "status","time_index", "entity_id", "entity_type", "fiware_servicepath" FROM "mtekz"."etparkingspot" WHERE "time_index"<? AND "entity_id"=? AND "fiware_servicepath"=? ORDER BY time_index DESC', (previousDay.strftime('%s')+'000', entity, path))
+            # Getting the last known status. Assumed free if none found
+            cursor.execute('SELECT "status","time_index", "entity_id", "entity_type", "fiware_servicepath" FROM "mtekz"."etparkingspot" WHERE "time_index"<? AND "entity_id"=? AND "fiware_servicepath"=? AND status!=? ORDER BY time_index DESC', (previousDay.strftime('%s')+'000', entity, path, 'None'))
             previousState = cursor.fetchone()
             if previousState:
                 previousState = previousState[0]
@@ -39,6 +49,7 @@ def main(argv):
                 entity_type = entityData[0][3]
             else:
                 entity_type = None
+            # For each of the 24 hours of the past day
             for i in range(24):
                 start_time = (currentTime - datetime.timedelta(hours=(24-i))).strftime('%s')+'000'
                 end_time = (currentTime - datetime.timedelta(hours=(24-i-1))).strftime('%s')+'000'
@@ -54,10 +65,8 @@ def main(argv):
                         timePassed = hourData[j+1][1] - hourData[j][1]
                     if previousState == 'occupied':
                         occupiedTime = occupiedTime + timePassed
-                    if j != (len(hourData)-1):
+                    if j != (len(hourData)-1) and hourData[j+1][0]:
                         previousState = hourData[j+1][0]
-                    if not previousState:
-                        previousState = 'free'
                 occupancy = int(math.ceil((occupiedTime/3600000.0)*100))
                 occupancyData.append((occupancy, start_time, entity, entity_type, path))
     cursor.executemany('INSERT INTO "mtekz"."etparkingoccupancy" (occupancy, time_index, entity_id, entity_type, fiware_servicepath) VALUES (?,?,?,?,?)', occupancyData)
