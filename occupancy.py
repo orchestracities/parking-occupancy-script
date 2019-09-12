ae61f900b548875e8e4a846115fd84893f927e6c
cerfoglg <gabriele.cerfoglio@martel-innovate.com>
2019-09-12 17:16:11 +0200

    Fixes to issues with query limits of the script, other fixes to state tracking

--- occupancy.py
+++ occupancy.py
@@ -27,13 +27,14 @@ def main(argv):
             end_date = parser.parse(arg)
     connection = client.connect(crate_host, username=crate_user)
     cursor = connection.cursor()
+    comoputeOccupancy(cursor, start_date, end_date)
 
-    # Getting current time, and previous time. If no start date given, it's the last 24 hours
-    currentTime = datetime.datetime.now().replace(microsecond=0,second=0,minute=0,hour=0,tzinfo=pytz.UTC)
-
+def comoputeOccupancy(cursor, start_date, end_date):
     # Current time changed if end date specified
     if end_date:
         currentTime = end_date.replace(microsecond=0,second=0,minute=0,hour=0,tzinfo=pytz.UTC)
+    else:
+        currentTime = datetime.datetime.now().replace(microsecond=0,second=0,minute=0,hour=0,tzinfo=pytz.UTC)
 
     # Start time changed if given a start date
     if start_date:
@@ -50,7 +51,7 @@ def main(argv):
         sys.exit(2)
 
     # Setting up cursor and pulling data since the start time
-    cursor.execute('SELECT "status","time_index", "entity_id", "entity_type", "fiware_servicepath" FROM "mtekz"."etparkingspot" WHERE "time_index">=? ORDER BY time_index ASC', (previousTime.strftime('%s')+'000',))
+    cursor.execute('SELECT "status","time_index", "entity_id", "entity_type", "fiware_servicepath", "name", "refdevice" FROM "mtekz"."etparkingspot" WHERE "time_index">=? AND "time_index"<=? AND status!=? AND status!=? ORDER BY time_index ASC LIMIT 1000000', (previousTime.strftime('%s')+'000', currentTime.strftime('%s')+'000', 'None', 'unknown'))
     data = cursor.fetchall()
 
     # List of Service Paths
@@ -63,17 +64,17 @@ def main(argv):
         entityIds = list(dict.fromkeys(map(lambda a: a[2], pathData)))
         for entity in entityIds:
             # Getting the last known status. Assumed free if none found
-            cursor.execute('SELECT "status","time_index", "entity_id", "entity_type", "fiware_servicepath" FROM "mtekz"."etparkingspot" WHERE "time_index"<? AND "entity_id"=? AND "fiware_servicepath"=? AND status!=? ORDER BY time_index DESC', (previousTime.strftime('%s')+'000', entity, path, 'None'))
-            previousState = cursor.fetchone()
-            if previousState:
-                previousState = previousState[0]
+            cursor.execute('SELECT "status","time_index", "entity_id", "entity_type", "fiware_servicepath", "name", "refdevice" FROM "mtekz"."etparkingspot" WHERE "time_index"<? AND "entity_id"=? AND "fiware_servicepath"=? AND status!=? AND status!=? ORDER BY time_index DESC', (previousTime.strftime('%s')+'000', entity, path, 'None', 'unknown'))
+            previousStateRow = cursor.fetchone()
+            if previousStateRow:
+                previousState = previousStateRow[0]
             else:
                 previousState = 'free'
             entityData = filter(lambda a: a[2] == entity, pathData)
-            if entityData and len(entityData) > 0:
-                entity_type = entityData[0][3]
-            else:
-                entity_type = None
+            # Setting these to None as defaults
+            entity_type = None
+            name = None
+            refdevice = None
             # For each of the hours since the start time given
             for i in range(hoursDiff):
                 start_time = (currentTime - datetime.timedelta(hours=(hoursDiff-i+1))).strftime('%s')+'000'
@@ -82,6 +83,13 @@ def main(argv):
                 occupiedTime = 0
                 if len(hourData) > 0:
                     for j in range((len(hourData)+1)):
+                        if j != len(hourData):
+                            if hourData[j][3]:
+                                entity_type = hourData[j][3]
+                            if hourData[j][5]:
+                                name = hourData[j][5]
+                            if hourData[j][6]:
+                                refdevice = hourData[j][6]
                         timePassed = 0
                         if j == 0:
                             timePassed = hourData[j][1] - long(start_time)
@@ -95,10 +103,12 @@ def main(argv):
                             previousState = hourData[j][0]
                 if len(hourData) == 0 and previousState == 'occupied':
                     occupiedTime = 3600000
+                if len(hourData) == 0 and previousState == 'free':
+                    occupiedTime = 0
                 occupancy = int(math.ceil((occupiedTime/3600000.0)*100))
-                timezonedStartTime = datetime.datetime.fromtimestamp(long(start_time)/1000.0).replace(tzinfo=pytz.utc).astimezone(get_localzone()).strftime('%s')+'000'
-                occupancyData.append((occupancy, timezonedStartTime, entity, entity_type, path))
-    cursor.executemany('INSERT INTO "mtekz"."etparkingoccupancy" (occupancy, time_index, entity_id, entity_type, fiware_servicepath) VALUES (?,?,?,?,?)', occupancyData)
+                timezonedStartTime = datetime.datetime.fromtimestamp(long(start_time)/1000.0).strftime('%s')+'000'
+                occupancyData.append((occupancy, timezonedStartTime, entity, entity_type, path, name, refdevice))
+    cursor.executemany('INSERT INTO "mtekz"."etparkingoccupancy" (occupancy, time_index, entity_id, entity_type, fiware_servicepath, name, refdevice) VALUES (?,?,?,?,?,?,?)', occupancyData)
     sys.exit()
 
 if __name__ == "__main__":
