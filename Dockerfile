6cfaa2e6246530206a0b6ea3a49d2e5a9e858bb7
Gabriele Cerfoglio <gabrielecerfoglio225@gmail.com>
2022-11-10 16:02:28 +0100

    Updated script for postgres, Python 3.11

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
-FROM python:2.7-alpine
+FROM python:3.11-alpine
+
+RUN apk add --no-cache postgresql-libs && \
+    apk add --no-cache --virtual .build-deps gcc musl-dev postgresql-dev
 
 RUN mkdir /app
 
--- occupancy.py
+++ occupancy.py
@@ -6,23 +6,21 @@ import logging
 import math
 import pytz
 import sys
-
-from crate import client
+import psycopg2
 from dateutil import parser
-
 
 def main(argv):
     logging.basicConfig()
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     # Setting up the connection to CrateDB (with command line args)
-    crate_host = None
-    crate_user = None
-    crate_password = None
+    pg_host = None
+    pg_user = None
+    pg_password = None
     start_date = None
     end_date = None
     delta = 24
-    tenant_name = 'EKZ'
+    tenant_name = 'dietikon'
     cursor = None
     connection = None
     dry_run = False
@@ -33,15 +31,15 @@ def main(argv):
                                     "end-date=", "delta-time", "dry-run"])
     except getopt.GetoptError:
         logger.error("wrong parameters")
-        print('occupancy.py -h <cratedb_host> -u <cratedb_user>')
+        print('occupancy.py -h <db_host> -u <db_user>')
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-h", "--host"):
-            crate_host = arg
+            pg_host = arg
         elif opt in ("-u", "--user"):
-            crate_user = arg
+            pg_user = arg
         elif opt in ("-p", "--password"):
-            crate_password = arg
+            pg_password = arg
         elif opt in ("-t", "--tenant-name"):
             tenant_name = parser.parse(arg)
         elif opt in ("-s", "--start-date"):
@@ -52,15 +50,18 @@ def main(argv):
             delta = int(arg)
         elif opt in ("-r", "--dry-run"):
             dry_run = bool(arg)
-    if not crate_host:
+    if not pg_host:
         logger.error("missing parameters")
         print('occupancy.py -h <cratedb_host> -u <cratedb_user>')
         sys.exit(-2)
     try:
         logger.info("connecting...")
         schema = "mt" + tenant_name.lower()
-        connection = client.connect(crate_host, username=crate_user,
-                                    password=crate_password)
+        connection = psycopg2.connect(database="quantumleap",
+                        host=pg_host,
+                        user=pg_user,
+                        password=pg_password,
+                        port=5432)
         cursor = connection.cursor()
         computeOccupancy(cursor, schema, start_date, end_date, delta, dry_run)
     except Exception as e:
@@ -108,16 +109,15 @@ def computeOccupancy(cursor, schema, start_date, end_date, delta, dry_run):
     limit = 1000
     offset = 0
     data = []
-    query = 'SELECT "status","time_index", "entity_id", "entity_type", ' \
-           '"fiware_servicepath", "name", "refdevice" FROM ' \
-           '"{}"."etparkingspot" WHERE "time_index">=? AND "time_index"<=? ' \
-           'AND status!=? AND status!=? ORDER BY time_index ASC LIMIT {} ' \
-           'OFFSET {}'
+    query = "SELECT status, time_index, entity_id, entity_type, " \
+           "fiware_servicepath, name, refdevice FROM " \
+           "{}.etparkingspot WHERE time_index>='{}' AND time_index<='{}' " \
+           "AND status!='None' AND status!='unknown' ORDER BY time_index ASC LIMIT {} " \
+           "OFFSET {}"
     while offset >= 0:
-        stmt = query.format(schema, limit, offset)
-        cursor.execute(stmt,
-            (previousTime.isoformat(),
-             currentTime.isoformat(), 'None', 'unknown'))
+        stmt = query.format(schema, previousTime.isoformat(), currentTime.isoformat(), limit, offset)
+        print(stmt)
+        cursor.execute(stmt)
         current_size = len(data)
         data += cursor.fetchall()
         if len(data) == current_size:
@@ -132,20 +132,16 @@ def computeOccupancy(cursor, schema, start_date, end_date, delta, dry_run):
     # Computing occupancy data for all entities and servicepaths
     occupancyData = []
     for path in servicePaths:
-        pathData = filter(lambda a: a[4] == path, data)
+        pathData = list(filter(lambda a: a[4] == path, data))
         entityIds = list(dict.fromkeys(map(lambda a: a[2], pathData)))
         for entity in entityIds:
             # Getting the last known status. Assumed free if none found
-            stmt = 'SELECT "status","time_index", "entity_id", ' \
-                   '"entity_type", "fiware_servicepath", "name", "refdevice" ' \
-                   'FROM "{}"."etparkingspot" WHERE "time_index"<? AND ' \
-                   '"entity_id"=? AND "fiware_servicepath"=? AND status!=? ' \
-                   'AND status!=? ORDER BY time_index DESC LIMIT 1'.format(schema)
-
-            cursor.execute(
-                stmt,
-                (previousTime.strftime('%s') + '000', entity, path, 'None',
-                 'unknown'))
+            stmt = "SELECT status, time_index, entity_id, " \
+                   "entity_type, fiware_servicepath, name, refdevice " \
+                   "FROM {}.etparkingspot WHERE time_index<'{}' AND " \
+                   "entity_id='{}' AND fiware_servicepath='{}' AND status!='None' " \
+                   "AND status!='unknown' ORDER BY time_index DESC LIMIT 1".format(schema, previousTime, entity, path)
+            cursor.execute(stmt)
             previousStateRow = cursor.fetchone()
             if previousStateRow:
                 previousState = previousStateRow[0]
@@ -157,13 +153,13 @@ def computeOccupancy(cursor, schema, start_date, end_date, delta, dry_run):
                 entity_type = None
                 name = None
                 refdevice = None
-            entityData = filter(lambda a: a[2] == entity, pathData)
+            entityData = list(filter(lambda a: a[2] == entity, pathData))
             occupancyData.extend(computeEntityOccupancy(entity, entity_type, name, path, refdevice, entityData, previousState, previousTime, hoursDiff))
 
     logger.info("occupancy computed")
     if dry_run:
         logger.info("dry run mode, no data will be stored")
-    stmt = 'INSERT INTO "{}"."etparkingoccupancy" (occupancy, time_index, entity_id, entity_type, fiware_servicepath, name, refdevice) VALUES (?,?,?,?,?,?,?)'.format(schema)
+    stmt = "INSERT INTO {}.etparkingoccupancy (occupancy, time_index, entity_id, entity_type, fiware_servicepath, name, refdevice) VALUES (?,?,?,?,?,?,?)".format(schema)
     for i in range(0, len(occupancyData), 1000):
         chunck = occupancyData[i:i + 1000]
         if not dry_run:
@@ -180,13 +176,13 @@ def computeEntityOccupancy(entity, entity_type, name, path, refdevice, entityDat
     # For each of the hours since the start time given
     for i in range(hoursDiff):
         start_time = (previousTime + datetime.timedelta(
-            hours=i)).strftime('%s') + '000'
+            hours=i))
         end_time = (previousTime + datetime.timedelta(
-            hours=(i + 1))).strftime('%s') + '000'
-        hourData = filter(
-            lambda d: d[1] >= int(start_time) and d[1] < int(end_time),
-            entityData)
-        occupiedTime = 0
+            hours=(i + 1)))
+        hourData = list(filter(
+            lambda d: d[1] >= start_time and d[1] < end_time,
+            entityData))
+        occupiedTime = datetime.timedelta(hours=0)
         if len(hourData) > 0:
             for j in range((len(hourData) + 1)):
                 if j != len(hourData):
@@ -196,11 +192,11 @@ def computeEntityOccupancy(entity, entity_type, name, path, refdevice, entityDat
                         name = hourData[j][5]
                     if hourData[j][6]:
                         refdevice = hourData[j][6]
-                timePassed = 0
+                timePassed = datetime.timedelta(hours=0)
                 if j == 0:
-                    timePassed = hourData[j][1] - long(start_time)
+                    timePassed = hourData[j][1] - start_time
                 elif j == len(hourData):
-                    timePassed = long(end_time) - hourData[j - 1][1]
+                    timePassed = end_time - hourData[j - 1][1]
                 else:
                     timePassed = hourData[j][1] - hourData[j - 1][1]
                 if previousState == 'occupied':
@@ -208,13 +204,11 @@ def computeEntityOccupancy(entity, entity_type, name, path, refdevice, entityDat
                 if j != len(hourData) and hourData[j][0]:
                     previousState = hourData[j][0]
         if len(hourData) == 0 and previousState == 'occupied':
-            occupiedTime = 3600000
+            occupiedTime = datetime.timedelta(hours=1)
         if len(hourData) == 0 and previousState == 'free':
-            occupiedTime = 0
-        occupancy = round(math.ceil((occupiedTime / 3600000.0) * 100),2)
-        timezonedStartTime = datetime.datetime.fromtimestamp(
-            long(start_time) / 1000.0)
-        timezonedStartTime = timezonedStartTime.replace(
+            occupiedTime = datetime.timedelta(hours=0)
+        occupancy = occupiedTime.total_seconds() * 1000
+        timezonedStartTime = start_time.replace(
             tzinfo=pytz.UTC).isoformat()
         logger.debug("entity {} in path {} occupancy computed is {} "
                      "on time {}".format(entity, path, occupancy,
